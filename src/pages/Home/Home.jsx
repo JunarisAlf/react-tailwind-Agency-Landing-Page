14e8cf0b9321bd9dea7841565f818558b0582679
JunarisAlf <junaris0715@gmail.com>
2022-05-29 20:08:18 +0700

    init

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+function Home() {
+  return (
+    <div className="App">
+      <span className="text-3xl font-bold underline"> Hallo World</span>
+    </div>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/components/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/pages/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
