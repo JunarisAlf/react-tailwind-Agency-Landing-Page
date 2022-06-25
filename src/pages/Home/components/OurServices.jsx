8bcb936b1aba2fac2bb1f6f764acf56658d1c22e
JunarisAlf <junaris0715@gmail.com>
2022-06-25 14:06:16 +0700

    Adding Responsive to Our Services

--- src/pages/Home/components/OurServices.jsx
+++ src/pages/Home/components/OurServices.jsx
@@ -1,17 +1,17 @@
-import arrow from '@/assets/icons/arrow-right.svg'
-import ServiceItem from '@/pages/Home/components/ServiceItem'
-import ItemOne from '@/assets/icons/monitor.svg'
-import ItemTwo from '@/assets/icons/settings.svg'
-import ItemThree from '@/assets/icons/pen-tool.svg'
-import ItemFour from '@/assets/icons/tv.svg'
+import arrow from '@/assets/icons/arrow-right.svg';
+import ServiceItem from '@/pages/Home/components/ServiceItem';
+import ItemOne from '@/assets/icons/monitor.svg';
+import ItemTwo from '@/assets/icons/settings.svg';
+import ItemThree from '@/assets/icons/pen-tool.svg';
+import ItemFour from '@/assets/icons/tv.svg';
 
 export default function OurServices() {
     return (
-        <section id="services" >
-            <h2 className="mt-24 font-quicksand text-[0.85rem] text-blue text-center font-semibold">
-                Our Services
-            </h2>
-            <div>
+        <section id="services" className="mt-32 flex flex-col lg:flex-row">
+            <div className="lg:flex-none lg:w-[45%]">
+                <h2 className="font-quicksand text-[0.85rem] text-blue text-center font-semibold">
+                    Our Services
+                </h2>
                 <h3 className="font-nunito text-[1.5rem] text-black text-center font-bold">
                     Perfect and Fast Movement
                 </h3>
@@ -22,15 +22,29 @@ export default function OurServices() {
                     your business
                 </p>
                 <div className="mt-2 flex flex-row gap-2 items-center">
-                    <span className="font-quicksand text-[0.85rem] text-blue font-bold">Read More </span>
-                    <img src={arrow} alt="arrow icon" className="w-6 aspect-square"/>
+                    <span className="font-quicksand text-[0.85rem] text-blue font-bold">
+                        Read More{' '}
+                    </span>
+                    <img
+                        src={arrow}
+                        alt="arrow icon"
+                        className="w-6 aspect-square"
+                    />
                 </div>
-                <div className="flex flex-row flex-wrap gap-5 mt-6 content-center">
-                    <ServiceItem icon={ItemOne} text="Social Media Management" bgColor='#377DFF'/>
-                    <ServiceItem icon={ItemTwo} text="Search Engine Opimization" bgColor='#FF2D2D'/>
-                    <ServiceItem icon={ItemThree} text="Design" bgColor='#7CE761'/>
-                    <ServiceItem icon={ItemFour} text="Ads" bgColor='#FFA800'/>
-                </div>
+            </div>
+            <div className="mt-6 lg:-mt-6 flex lg:flex-none lg:w-[55%] flex-row flex-wrap content-center justify-around">
+                <ServiceItem
+                    icon={ItemOne}
+                    text="Social Media Management"
+                    bgColor="#377DFF"
+                />
+                <ServiceItem
+                    icon={ItemTwo}
+                    text="Search Engine Opimization"
+                    bgColor="#FF2D2D"
+                />
+                <ServiceItem icon={ItemThree} text="Design" bgColor="#7CE761" />
+                <ServiceItem icon={ItemFour} text="Ads" bgColor="#FFA800" />
             </div>
         </section>
     );
--- src/pages/Home/components/ServiceItem.jsx
+++ src/pages/Home/components/ServiceItem.jsx
@@ -1,14 +1,14 @@
 export default function ServiceItem({ icon, text, bgColor }) {
     const classBgColor = `bg-[${bgColor}]`;
     return (
-        <div className="mx-auto px-10 flrx-auto flex flex-col gap-4 items-center justify-center w-1/3 min-w-[15rem] aspect-square rounded-2xl bg-white drop-shadow-spread border border-1 border-grey border-opacity-20">
+        <div className="mx-auto mt-5 px-10  flex flex-col gap-4 items-center justify-center w-1/3 min-w-[15rem] aspect-square rounded-2xl bg-white drop-shadow-spread border border-1 border-grey border-opacity-20 lg:min-w-[11rem] lg:mx-0">
             <div
-                className="flex w-[6.25rem] aspect-square rounded-full"
+                className="flex w-[6.25rem] lg:w-[4rem] aspect-square rounded-full"
                 style={{backgroundColor: bgColor}}
             >
                 <img src={icon} alt="icon" className="mx-auto self-center " />
             </div>
-            <span className="font-quicksand text-[1rem] font-bold text-black text-center leading-6">
+            <span className="font-quicksand text-[1rem] lg:text-[0.8rem] font-bold text-black text-center leading-6">
                 {text}
             </span>
         </div>
